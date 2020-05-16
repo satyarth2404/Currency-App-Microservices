21cbfb511b696e0e66e233647babb09a9bef0fb4
satyarth2404 <shukla.satyarth99@gmail.com>
2020-05-16 23:10:03 +0530

    First Commit (Spring Cloud Bus Missing)

---
+++ limits-service/src/main/java/com/saty/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,31 @@
+package com.saty.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.saty.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		 return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("not available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999,9);
+	}
+	
+}
